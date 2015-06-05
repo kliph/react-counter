a1360c1ad4212d75f67f0a7ebca8dc8339b4e549
Cliff Rodgers <kliph@users.noreply.github.com>
2015-06-05 10:32:12 -0500

    Use some sweet arrows

--- public/script.js
+++ public/script.js
@@ -2,13 +2,13 @@ var Counter = React.createClass({
     render: function() {
         return (
           <div className="counter">
-          <div className="increment-button">
-          <button>^</button>
-          </div>
-          0
-          <div className="decrement-button">
-          <button>v</button>
-          </div>
+            <div className="increment-button" onClick={this.incCounter}>
+              <button>⇮</button>
+            </div>
+              0
+            <div className="decrement-button" onClick={this.decCounter}>
+              <button>⇮</button>
+            </div>
           </div>
 
         );
