8a97984d5d836872d1ba83d74ba785b8b8adde08
Cliff Rodgers + spinningtopsofdoom <dev+kliph+spinningtopsofdoom@bendyworks.com>
2015-06-05 09:34:50 -0500

    Mock up basic counter layout

--- public/script.js
+++ public/script.js
@@ -1,9 +1,16 @@
 var Counter = React.createClass({
     render: function() {
         return (
-                <div className="counter">
-                I'm a Counter, hello there.
-</div>
+          <div className="counter">
+          <div className="increment-button">
+          <button>^</button>
+          </div>
+          0
+          <div className="decrement-button">
+          <button>v</button>
+          </div>
+          </div>
+
         );
     }
 });
