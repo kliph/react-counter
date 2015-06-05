4e1baf38b539b4fd00f9c3be286d2f5f87a32886
Cliff Rodgers + spinningtopsofdoom <dev+kliph+spinningtopsofdoom@bendyworks.com>
2015-06-05 11:28:05 -0500

    Counter counts up and down
    - Track counter state in App component

--- public/script.js
+++ public/script.js
@@ -1,12 +1,31 @@
+var App = React.createClass({
+  getInitialState: function() {
+    return {counter: 0};
+  },
+  incCounter: function() {
+    this.setState({counter: (this.state.counter + 1)})
+  },
+  decCounter: function() {
+    this.setState({counter: (this.state.counter - 1)})
+  },
+  render: function() {
+    return (
+    <div className="app">
+      <Counter counter={this.state.counter} dec={this.decCounter} inc={this.incCounter} />
+    </div>
+    );
+  }
+});
+
 var Counter = React.createClass({
     render: function() {
         return (
           <div className="counter">
-            <div className="increment-button" onClick={this.incCounter}>
+            <div className="increment-button" onClick={this.props.inc}>
               <button>⇮</button>
             </div>
-              0
-            <div className="decrement-button" onClick={this.decCounter}>
+              {this.props.counter}
+            <div className="decrement-button" onClick={this.props.dec}>
               <button>⇮</button>
             </div>
           </div>
@@ -16,6 +35,6 @@ var Counter = React.createClass({
 });
 
 React.render(
-<Counter />,
-document.getElementById('content')
+  <App />,
+  document.getElementById('content')
 );
